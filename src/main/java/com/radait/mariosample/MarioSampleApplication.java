3864618f99625b1dc649ab151a2e3c4f3b9deb40
hm lim <limhunmuk@gmail.com>
2025-09-03 16:54:53 +0900

    시큐리티/jwt 인증 추가

--- src/main/java/com/radait/mariosample/MarioSampleApplication.java
+++ src/main/java/com/radait/mariosample/MarioSampleApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarioSampleApplication {
 
     public static void main(String[] args) {
-        System.out.println("test");
         SpringApplication.run(MarioSampleApplication.class, args);
     }
 
